56282f3005668da8d321c9985506ee7672123eaf
RoniKhizverg <68865093+RoniKhizverg@users.noreply.github.com>
2022-02-10 14:08:29 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MetaCentraland from './MetaCentraland/MetaCentraland'
-import './MetaCentraland/MetaCentraland.css'
-
-
-ReactDOM.render( <
-    MetaCentraland / > , document.getElementById('root')
-);
