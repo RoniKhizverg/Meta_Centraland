d5700bff4486db2fbee81dfd1726b3de3151b3c0
RoniKhizverg <khizverg@gmail.com>
2022-03-01 11:15:49 +0200

    put backend folder outside the front

---
+++ backend/models/log-In.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const signUpScheme = new mongoose.Schema({
+
+
+        ID: {
+            type: String,
+            required: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+        userType: {
+            type: String,
+            required: true
+        }
+
+    }) //what are the details we want to collect from the user when they buy plots
+module.exports = mongoose.model('login', signUpScheme)
